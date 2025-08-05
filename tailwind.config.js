1ebaad0d8c3946977f6d515b2896c33dc7eee3b9
DNC-DiamondNuttedNation <diamondnuttedchads@gmail.com>
2025-08-05 17:06:45 -0500

    more tailwind fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
